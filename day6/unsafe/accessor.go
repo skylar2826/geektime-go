d878b86b601edbf2dd8933f56847a95f686e2920
zhangluyue <zhangluyue@qianxin.com>
2024-09-13 10:13:51 +0800

    feat: 手写accessor

--- day6/unsafe/accessor.go
+++ day6/unsafe/accessor.go
@@ -6,35 +6,34 @@ import (
 	"unsafe"
 )
 
-type UnsafeAccessor struct {
-	Fields  map[string]*FieldMeta
+type Accessor struct {
+	Fields  map[string]*Field
 	address unsafe.Pointer
 }
 
-func NewUnsafeAccessor(entity any) *UnsafeAccessor {
+func NewUnsafeAccessor(entity any) *Accessor {
 	typ := reflect.TypeOf(entity).Elem()
-
 	numField := typ.NumField()
-	fields := make(map[string]*FieldMeta, numField)
 
-	val := reflect.ValueOf(entity)
-
+	fields := make(map[string]*Field, numField)
 	for i := 0; i < numField; i++ {
 		field := typ.Field(i)
-		fields[field.Name] = &FieldMeta{
+		fields[field.Name] = &Field{
 			offset: field.Offset,
 			typ:    field.Type,
 		}
 	}
 
-	return &UnsafeAccessor{
-		Fields: fields,
-		// 	val.UnsafeAddr() 返回uintptr是个值，GC（垃圾回收)后，address改变，uintptr失效; val.UnsafePointer 返回 unsafe.pointer是一个指针，GC会维护更新它的值
+	val := reflect.ValueOf(entity)
+	accessor := &Accessor{
+		Fields:  fields,
 		address: val.UnsafePointer(),
 	}
+
+	return accessor
 }
 
-func (u *UnsafeAccessor) Field(name string) (any, error) {
+func (u *Accessor) Field(name string) (any, error) {
 	field, ok := u.Fields[name]
 	if !ok {
 		return nil, fmt.Errorf("field %s not found", name)
@@ -42,24 +41,23 @@ func (u *UnsafeAccessor) Field(name string) (any, error) {
 
 	fieldAddress := unsafe.Pointer(uintptr(u.address) + field.offset)
 
-	// 知道地址 =》 把地址转化成*对象
 	return reflect.NewAt(field.typ, fieldAddress).Elem().Interface(), nil
-	// 已知返回类型
-	//return *(*int)(fieldAddress), nil
 }
 
-func (u *UnsafeAccessor) SetField(name string, value any) error {
+func (u *Accessor) SetField(name string, value any) error {
 	field, ok := u.Fields[name]
+
 	if !ok {
 		return fmt.Errorf("field %s not found", name)
 	}
+
 	fieldAddress := unsafe.Pointer(uintptr(u.address) + field.offset)
+
 	reflect.NewAt(field.typ, fieldAddress).Elem().Set(reflect.ValueOf(value))
 	return nil
 }
 
-type FieldMeta struct {
-	//Offset unsafe.Pointer
+type Field struct {
 	offset uintptr
 	typ    reflect.Type
 }

3d59ebb4edc39e8257eb63532156faf1ea9b5de5
zhangluyue <zhangluyue@qianxin.com>
2024-11-11 11:42:18 +0800

    feat: 连接池开发

--- day12/pool.go
+++ day12/pool.go
@@ -9,59 +9,48 @@ import (
 )
 
 type Pool struct {
-	// 空闲队列
-	idlesConns chan *idleConn
-	// 请求队列
-	reqConn []connReq
-
-	// 最大连接数
-	maxCnt int
-	// 当前连接数
-	cnt int
-	// 初始连接数
-	initCnt int
-
-	// 最大连接时间
-	maxIdleTime time.Duration
-
-	factory func() (net.Conn, error)
-
-	lock sync.Mutex
+	idleConns   chan *idleConn // 空闲队列 chan的容量 =》 最大空闲数量
+	reqQueue    []connReq      // 等待队列，不使用chan原因：chan有最大容量，超过容量的会被进一步阻塞
+	maxCnt      int            // 最大连接数
+	cnt         int            // 当前连接数
+	maxIdleTime time.Duration  // 最大超时时间
+	factory     func() (net.Conn, error)
+	lock        sync.Mutex
 }
 
-type idleConn struct {
-	c              net.Conn
-	lastActiveTime time.Time
-}
-
-type connReq struct {
-	connChan chan net.Conn
-}
-
-func NewPool(maxCnt int, maxIdleCnt int, maxIdleTime time.Duration, initCnt int, factory func() (net.Conn, error)) (*Pool, error) {
-	idleConns := make(chan *idleConn, maxIdleCnt)
-
-	if initCnt > maxIdleCnt {
-		return nil, errors.New("初始连接数不能大于最大连接数")
+func NewPool(initCnt int, maxIdleConns int, maxCnt int, maxIdleTime time.Duration, factory func() (net.Conn, error)) (*Pool, error) {
+	if initCnt > maxCnt {
+		return nil, errors.New("初始连接数大于最大连接数")
 	}
 
-	for i := 0; i < initCnt; i++ {
+	idleConns := make(chan *idleConn, maxIdleConns)
+	for i := 0; i <= initCnt; i++ {
 		conn, err := factory()
 		if err != nil {
 			return nil, err
 		}
-		idleConns <- &idleConn{c: conn, lastActiveTime: time.Now()}
+		idleConns <- &idleConn{
+			conn:           conn,
+			lastActiveTime: time.Now(),
+		}
 	}
 
 	res := &Pool{
-		idlesConns:  idleConns,
+		idleConns:   idleConns,
 		maxCnt:      maxCnt,
-		cnt:         0,
 		maxIdleTime: maxIdleTime,
 		factory:     factory,
 	}
-
 	return res, nil
+}
+
+type idleConn struct {
+	conn           net.Conn
+	lastActiveTime time.Time
+}
+
+type connReq struct {
+	conn chan net.Conn
 }
 
 func (p *Pool) Get(ctx context.Context) (net.Conn, error) {
@@ -74,72 +63,81 @@ func (p *Pool) Get(ctx context.Context) (net.Conn, error) {
 
 	for {
 		select {
-		case ic := <-p.idlesConns:
-			// 拿到空闲连接
-			// 判断是否超时
-			if ic.lastActiveTime.Add(p.maxIdleTime).Before(time.Now()) {
-				_ = ic.c.Close()
+		case i := <-p.idleConns:
+			// 存在空闲连接
+			// 检查是否超时
+			if i.lastActiveTime.Add(p.maxIdleTime).Before(time.Now()) {
+				// 超时，获取下一个空闲连接
 				continue
 			}
-			return ic.c, nil
+			return i.conn, nil
 		default:
+			// 不存在空闲连接
 			p.lock.Lock()
+
 			if p.cnt >= p.maxCnt {
-				req := connReq{connChan: make(chan net.Conn, 1)}
-				p.reqConn = append(p.reqConn, req)
-				p.lock.Unlock()
-				// 没有空闲连接
+				// 超过最大数量
+				cReq := connReq{
+					conn: make(chan net.Conn, 1),
+				}
+
+				p.reqQueue = append(p.reqQueue, cReq)
+
 				select {
-				case <-ctx.Done():
-					// 超时
-					// 选项1：从req中删除自己（不行，没得删）
-					// 选项2：转发
-					go func() {
-						c := <-req.connChan
-						_ = p.Put(ctx, c)
-					}()
-					return nil, ctx.Err()
-				case c := <-req.connChan:
-					// 等别人归还
+				case c := <-cReq.conn:
+					// 等到别人归还
+					p.lock.Unlock()
 					return c, nil
+				case <-ctx.Done():
+					// 转发该空闲连接
+					go func() {
+						c := <-cReq.conn
+						_ = p.Put(context.Background(), c)
+					}()
+					p.lock.Unlock()
+					return nil, ctx.Err()
 				}
-			} else {
-				// 有空闲连接
-				c, err := p.factory()
-				if err != nil {
-					return nil, err
-				}
-				p.cnt++
-				p.lock.Unlock()
-				return c, nil
 			}
+
+			// 没有超过最大连接数 =》 创建连接
+			c, err := p.factory()
+			if err != nil {
+				return nil, err
+			}
+			p.cnt++
+			p.lock.Unlock()
+			return c, nil
 		}
-
 	}
-
 }
 
 func (p *Pool) Put(ctx context.Context, conn net.Conn) error {
-	p.lock.Lock()
-	if len(p.reqConn) > 0 {
-		// 取队尾元素，队首元素等待时间长，容易过期
-		req := p.reqConn[len(p.reqConn)-1]
-		p.reqConn = p.reqConn[:(len(p.reqConn) - 1)]
+	if len(p.reqQueue) > 0 {
+		// 有阻塞请求，唤醒阻塞请求
+		p.lock.Lock()
+		reqLen := len(p.reqQueue)
+		// 取队尾元素，队尾元素比队首元素后放入，大概率没有超期
+		req := p.reqQueue[reqLen-1]
+		p.reqQueue = p.reqQueue[:reqLen-1]
 		p.lock.Unlock()
-		req.connChan <- conn
+		req.conn <- conn
 		return nil
 	}
+
 	p.lock.Unlock()
-	ic := &idleConn{
-		c:              conn,
-		lastActiveTime: time.Now(),
+
+	idleC := &idleConn{
+		conn: conn,
 	}
 	select {
-	case p.idlesConns <- ic:
+	case p.idleConns <- idleC:
 	default:
 		// 空闲队列满了
 		_ = conn.Close()
+		p.lock.Lock()
 		p.cnt--
+		p.lock.Unlock()
 	}
+
 	return nil
 }
